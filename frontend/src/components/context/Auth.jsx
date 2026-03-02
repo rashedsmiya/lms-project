1b09ea43679a281afa8d9f85e00761901181c893
rashedsmiya <rashedmiyaofficial@gmail.com>
2026-03-03 04:38:59 +0600

    add

---
+++ frontend/src/components/context/Auth.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import { createContext } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const userInfo = localStorage.getItem('userInfoLms');
+    const [user, setUser] = useState(userInfo); 
+
+    const login = (user) => {
+        setUser(user);
+    }
+
+    const logout = () => {
+        localStorage.removeItem('userInfoLms');
+        setUser(null);
+    }
+
+    return (
+        <AuthContext.Provider value={{ user, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthProvider;
