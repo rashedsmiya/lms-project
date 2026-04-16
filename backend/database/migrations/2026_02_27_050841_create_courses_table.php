5aab5bfd572b46d2e861b49b607cdd6bdad0ffac
rashedsmiya <rashedmiyaofficial@gmail.com>
2026-04-17 00:27:35 +0600

    Update 2026_02_27_050841_create_courses_table.php

--- backend/database/migrations/2026_02_27_050841_create_courses_table.php
+++ backend/database/migrations/2026_02_27_050841_create_courses_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
                $table->id();
                $table->string('title');
                $table->foreignId('user_id')->constrained()->onDelete('cascade');
-               $table->foreignId('category_id')->nullable()->constrained()->onDelete('cascade');
                $table->foreignId('level_id')->nullable()->constrained()->onDelete('cascade');
                $table->foreignId('language_id')->nullable()->constrained()->onDelete('cascade');
                $table->text('description')->nullable();
